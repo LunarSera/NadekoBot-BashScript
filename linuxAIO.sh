5a18046c2f3c4f05ebecce7054610572ecda26ed
LunarSera <29496284+LunarSera@users.noreply.github.com>
2019-08-07 23:07:43 -0400

    rewrite for red

--- linuxAIO.sh
+++ linuxAIO.sh
@@ -1,33 +1,51 @@
 #!/bin/sh
 echo ""
-echo "Welcome to NadekoBot."
+echo "Welcome to Red Installer."
 
-choice=8
-	echo "1. Download Dev Build (Latest)"
-	echo "2. Download Stable Build"
-	echo "3. Run Nadeko (Normally)"
-	echo "4. Run Nadeko with Auto Restart (Run Nadeko normally before using this.)"
-	echo "5. Auto-Install Prerequisites (For Ubuntu, Debian and CentOS)"
-	echo "6. Set up credentials.json (If you have downloaded NadekoBot already.)"
-	echo "7. To Exit"
-	echo -n "Choose [1 or 2] to Download, [3 or 4] to Run, or [7] to Exit"
-while [ $choice -eq 8 ]; do
+choice=3
+	echo "1. Launch Red in PM2 (Only run this once or you will have more than one instance of the bot running.)"
+	echo "2. Install PMP and Pyvenv"
+	echo "3. Optimize Python for Red. (Only run once.)"
+	echo -n "Description of launcher."
+while [ $choice -eq 4 ]; do
 read choice
 if [ $choice -eq 1 ] ; then
 
 	echo ""
-	echo "Downloading NadekoBot, please wait."
-	curl -L https://github.com/Kwoth/NadekoBot-BashScript/raw/master/nadeko_installer_latest.sh | sh
+	echo "Installing Red, please wait."
+	pm2 start redbot --name Red --interpreter "/home/red/bin/python" -- red --no-prompt
 	echo ""
-	echo "NadekoBot Dev Build (Latest) downloaded."
-	bash linuxAIO.sh
+	echo "Redbot Installed. Monitoring now."
+	pm2 logs --lines 100
 else
 	if [ $choice -eq 2 ] ; then
 		echo ""
-		echo "Downloading NadekoBot, please wait."
-		curl -L https://github.com/Kwoth/NadekoBot-BashScript/raw/master/nadeko_installer.sh | sh
+		echo "Downloading PM2, please wait."
+		apt-get install -y build-essential &&
+		apt autoremove -y && 
+		apt-get install curl software-properties-common &&
+		curl -sL https://deb.nodesource.com/setup_10.x | -E bash -
+		apt-get install -y nodejs && 
+		curl -sL https://deb.nodesource.com/setup_10.x | -E bash - 
+		apt-get install -y npm &&
+		sleep 10 &&
+		echo Did npm install?
+		apt-get update &&
+		echo npm version:
+		npm --version
+		echo Should be 6.9.0 or newer &&
+		sleep 5 &&
+		apt-get install zlib1g-dev &&
+		npm install pm2 -g &&
+		apt install python3.7 python3.7-dev python3.7-venv python3-pip build-essential \
+		libssl-dev libffi-dev git unzip default-jre -y &&
+		sudo curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash &&
+		echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc &&
+		echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc &&
+		echo 'eval "$(pyenv init -)"' >> ~/.bashrc &&
+		exec bash
 		echo ""
-		echo "NadekoBot Stable Build downloaded."
+		echo "PM2 and pyvenv installed."
 		bash linuxAIO.sh
 	else
 		if [ $choice -eq 3 ] ; then
@@ -38,133 +56,14 @@ else
 			echo "Welcome back to NadekoBot."
 			bash linuxAIO.sh
 		else
-			if [ $choice -eq 4 ] ; then
-				echo ""
-				echo "Running Nadeko with Auto Restart you will have to close the session to stop the auto restart."
-				sleep 5s
-				wget -N https://github.com/Kwoth/NadekoBot-BashScript/raw/master/NadekoAutoRestartAndUpdate.sh && bash NadekoAutoRestartAndUpdate.sh
-				echo ""
-				echo "That did not work?"
-				bash linuxAIO.sh
-			else
-				if [ $choice -eq 5 ] ; then
-					echo ""
-					echo "Getting the Auto-Installer for Debian/Ubuntu"
-					wget -N https://github.com/Kwoth/NadekoBot-BashScript/raw/master/nadekoautoinstaller.sh && bash nadekoautoinstaller.sh
-					echo ""
-					echo "Welcome back..."
-					bash linuxAIO.sh
-				else
-					if [ $choice -eq 6 ] ; then
-						echo ""
-						echo
-echo -e "Let's begin creating a new credentials.json file if you are about to run the NadekoBot for the first time. \n \nPlease read JSON Explanations in the guide... \n \nPress [Y] when you are ready to continue or [N] to exit."
-while true; do
-    read -p "[y/n]: " yn
-    case $yn in
-        [Yy]* ) clear; break;;
-        [Nn]* ) echo Exiting...; exit;;
-        * ) echo "Couldn't get that please type [y] for Yes or [n] for No.";;
-    esac
-done
-clear
-cd NadekoBot/src/NadekoBot
-mv credentials.json credentials.json.old
 
-echo Please enter your bot client ID:
-read clientid
-echo Alright saved \'$clientid\' as your client ID.
-sleep 2
-clear
+			clear
+			echo "1. Download Dev Build (Latest)"
+			echo "2. Download Stable Build"
+			echo "3. Run Nadeko (Normally)"
 
-echo Please enter your bot ID \(should be same as your client ID\):
-read botid
-echo Alright saved \'$botid\' as your Bot ID.
-sleep 2
-clear
-
-echo Please enter your bot token \(It is not bot secret, it should be ~59 characters long.\):
-read token
-echo Alright saved \'$token\' as your bot\'s token.
-sleep 2
-clear
-
-echo Please enter your own ID \(Refer to the guide, it will be bot\'s owner ID.\):
-read ownerid
-echo Alright saved \'$ownerid\' as owner\'s ID.
-sleep 2
-clear
-
-echo Please enter Google API key \(Refer to the guide.\):
-read googleapi
-echo Alright saved \'$googleapi\' as your bot\'s Google API Key.
-sleep 2
-clear
-
-echo Please enter LoL API Key or Just Press [Enter Key] to skip. \(optional\) \(Refer to the JSON Explanations guide.\):
-read lolapikey
-echo Alright saved \'$lolapikey\'
-sleep 2
-clear
-
-echo Please enter Mashape Key or Just Press [Enter Key] to skip. \(optional\) \(Refer to the JSON Explanations guide.\):
-read mashapekey
-echo Alright saved \'$mashapekey\'
-sleep 2
-clear
-
-echo Please enter Osu API Key or Just Press [Enter Key] to skip. \(optional\) \(Refer to the JSON Explanations guide.\):
-read osu
-echo Alright saved \'$osu\'
-sleep 2
-clear
-
-echo Please enter SoundCloud Client Id or Just Press [Enter Key] to skip. \(optional\) \(Refer to the JSON Explanations guide.\):
-read scid
-echo Alright saved \'$scid\'
-sleep 2
-clear
-
-echo "{
-  \"ClientId\": $clientid,
-  \"BotId\": $botid,
-  \"Token\": \"$token\",
-  \"OwnerIds\": [
-    $ownerid
-  ],
-  \"LoLApiKey\": \"$lolapikey\",
-  \"GoogleApiKey\": \"$googleapi\",
-  \"MashapeKey\": \"$mashapekey\",
-  \"OsuApiKey\": \"$osu\",
-  \"SoundCloudClientId\": \"$scid\",
-  \"Db\": null,
-  \"TotalShards\": 1
-}" | cat - >> credentials.json
-echo Credentials setup completed.
-sleep 5
-clear
-cd -
-bash linuxAIO.sh
-					else
-						if [ $choice -eq 7 ] ; then
-							echo ""
-							echo "Exiting..."
-							exit 0
-						else
-							clear
-							echo "1. Download Dev Build (Latest)"
-							echo "2. Download Stable Build"
-							echo "3. Run Nadeko (Normally)"
-							echo "4. Run Nadeko with Auto Restart (Run Nadeko normally before using this.)"
-							echo "5. Auto-Install Prerequisites (For Ubuntu, Debian and CentOS)"
-							echo "6. Set up credentials.json (If you have downloaded NadekoBot already.)"
-							echo "7. To Exit"
-							echo -n "Choose [1 or 2] to Download, [3 or 4] to Run, or [7] to Exit."
-							choice=8
-						fi
-					fi
-				fi
-			fi
+			echo -n "Choose [1 or 2] to Download, [3 or 4] to Run, or [7] to Exit."
+			choice=8
 		fi
 	fi
 fi
